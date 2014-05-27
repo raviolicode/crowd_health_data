6eeb143e26c2c9a2617bab14c5123df20be22cd6
Lucia Escanellas <raviolicode@gmail.com>
2014-05-27 11:45:57 +0200

    Rails new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrowdHealthData::Application.config.secret_key_base = '7ef8dff2776e924dae184f2428bd01ecce0b0f69b9ada636e6689fad315b7aad22621b787ebaedb8ff7e90fd7d4f71e76ec9c196a9a00b9c7085bb92e9279da9'
