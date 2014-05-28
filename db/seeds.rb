62dba0fc113a8e00d0fbeee49f16e06b6d09b6cd
Lucia Escanellas <raviolicode@gmail.com>
2014-05-28 09:53:10 +0200

    Add Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+AdminUser.create([{ email: 'admin@example.com', password: 'password' }]) unless AdminUser.find_by_email('admin@example.com')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
